9ce2e355d8039e46eb35f0e88d140ba4fed5271c
sheikh-shishir <ahmed2305101594@diu.edu.bd>
2024-11-14 22:58:51 +0600

    second commit

--- src/main/java/com/mycompany/main/Main.java
+++ src/main/java/com/mycompany/main/Main.java
@@ -1,15 +1,8 @@
-
-
 package com.mycompany.main;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Date;
 
-/**
- *
- * @author macbookair
- */
 
 class Garment{
     public String id;
@@ -20,9 +13,21 @@ class Garment{
     public double price;
     public int stockQuantity ;
     
+    public Garment(String id,String name,String description,String size,String color,double price,int stockQuantity)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.size = size;
+        this.color = color;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        
+    }
+    
     public void updateStock(int quantity)
     {
-        stockQuantity = stockQuantity + 1;
+        this.stockQuantity = quantity;
         System.out.println("Current Stock is : "+stockQuantity);
     }
     public double calculateDiscountPrice(double discountPercentage)
@@ -36,6 +41,14 @@ class Fabric{
     public String type ;
     public String color;
     public double pricePerMeter;
+    
+    public Fabric(String id,String type,String color,double pricePerMeter)
+    {
+        this.id = id;
+        this.type = type;
+        this.color = color;
+        this.pricePerMeter = pricePerMeter;
+    }
     
     public double calculateCost(double meters)
     {
@@ -51,9 +64,16 @@ class  Supplier{
     public String contactInfo;
     private List<Fabric> suppliedFabrics = new ArrayList<>();
     
+    public Supplier(String id,String name,String contactInfo)
+    {
+        this.id = id;
+        this.name = name;
+        this.contactInfo = contactInfo;
+    }
+    
     public void addFabric(Fabric fabric) 
     {
-        
+        suppliedFabrics.add(fabric);
     }
     public List<Fabric> getSuppliedFabrics()
     {
@@ -63,9 +83,16 @@ class  Supplier{
 
 class Order{
     public String orderId ;
-    public Date orderDate ;
+    public String orderDate ;
     public List<Garment> garments = new ArrayList<>();
     public double totalAmount;
+    
+    public Order(String orderId,String orderDate,double totalAmount)
+    {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+    }
     
     public void addGarment(Garment garment) 
     {
@@ -80,12 +107,17 @@ class Order{
     }
     public void printOrderDetails() 
     {
+        System.out.println("--------------------------");
         System.out.println("Order Details : ");
+        System.out.println("--------------------------");
         for(Garment g : garments)
         {
             
             System.out.println("name : "+g.name);
             System.out.println("color : "+g.color);
+            System.out.println("Price : "+g.price);
+            System.out.println("Description : "+g.description);
+            System.out.println("--------------------------");
         }
     }
 }
@@ -99,7 +131,8 @@ class Customer
     
     public void placeOrder(Order order)
     {
-        
+        order.printOrderDetails();
+        System.out.println("Order Placed");
     }
     public List<Order> viewOrders() 
     {
@@ -120,7 +153,13 @@ class Inventory{
     }
     public Garment findGarment(String id)
     {
-        
+        for(Garment g : garments)
+        {
+            if(g.id == id)
+            {
+                return g;
+            }
+        }
         return null;
         
     }
@@ -131,14 +170,25 @@ public class Main {
 
     public static void main(String[] args) {
         
-        Garment g1 = new Garment();
-        g1.name = "silk";
-        g1.color = "red";
-        g1.price = 5;
-        g1.id ="1";
+        Garment g1 = new Garment("1","silk","good product","XL","Red",600,1000);
+        
         System.out.println(g1.calculateDiscountPrice(g1.price)) ;
+        
         g1.updateStock(15);
         
+        Fabric f1 = new Fabric("1","silk","blue",10);
+        System.out.println(f1.calculateCost(10));
+        
+        
+        Supplier s1 = new Supplier("1","12","01799809062");
+        s1.addFabric(f1);
+        System.out.println(s1.getSuppliedFabrics());
+        
+        Order o1 = new Order("100","1988-09-29",1000);
+        o1.addGarment(g1);
+        o1.printOrderDetails();
+        
+
          
     }
 }
